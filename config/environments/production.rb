d0ae0d9217a839c2bb62379dd83a88ce29c91411
Spenser Coke <hello@spenser.xyz>
2024-01-03 15:55:42 -0800

    host to

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,8 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
+  config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
+  config.action_mailer.asset_host = ENV.fetch("ASSET_HOST", ENV.fetch("APPLICATION_HOST"))
 
   ## Postmark
   # config.action_mailer.delivery_method = :postmark
